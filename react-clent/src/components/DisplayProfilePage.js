997e7705e8551f07cd8979375d646ddc8a1a2690
sjchae <sjchae416@gmail.com>
2022-04-19 14:17:53 +0900

    adjust profile page css

--- react-clent/src/components/DisplayProfilePage.js
+++ react-clent/src/components/DisplayProfilePage.js
@@ -1,9 +1,17 @@
 function DisplayProfilePage({ openModal }) {
-  return (
-    <button className="button-profile" id="button-profile" type="button" onClick={openModal}>
-      <img className="profile-picture" src={`${process.env.PUBLIC_URL}/assets/images/keyboard.jpg`} />
-    </button>
-  );
+	return (
+		<button
+			className="button-profile"
+			id="button-profile"
+			type="button"
+			onClick={openModal}
+		>
+			<img
+				className="profile-picture"
+				src={`${process.env.PUBLIC_URL}/assets/images/keyboard.jpg`}
+			/>
+		</button>
+	);
 }
 
 export default DisplayProfilePage;
