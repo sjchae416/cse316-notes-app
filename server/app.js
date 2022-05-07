d53a8e2c9c2d2d1ea50c9f3f3f234b35c7383afc
sjchae <sjchae416@gmail.com>
2022-05-08 01:20:22 +0900

    modify comments

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const Note = require('./models/note');
 const User = require('./models/user');
 const bodyParser = require('body-parser');
 const note = require('./models/note');
-const MongoStore = require('connect-mongo'); // MongoDB session store
+const MongoStore = require('connect-mongo');
 const session = require('express-session');
 
 const app = express();
@@ -14,24 +14,20 @@ app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Set up mongoose connection
-// var mongoDB = 'mongodb://localhost:27017/Notes'; // insert your database URL here
 var mongoDB =
-	'mongodb+srv://sjchae416:home612899@private.2anro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; // insert your database URL here
+	'mongodb+srv://sjchae416:home612899@private.2anro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const sessionSecret = 'make a secret string';
 
-// Create Mongo DB Session Store
 const store = MongoStore.create({
 	mongoUrl: mongoDB,
 	secret: sessionSecret,
 	touchAfter: 24 * 60 * 60,
 });
 
-// Setup to use the express-session package
 const sessionConfig = {
 	store,
 	name: 'session',
@@ -42,50 +38,39 @@ const sessionConfig = {
 		httpOnly: true,
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7,
-		// later you would want to add: 'secure: true' once your website is hosted on HTTPS.
 	},
 };
 
 app.use(session(sessionConfig));
 
-// This is middleware that will run before every request
 app.use((req, res, next) => {
-	// We can set variables on the request, which we can then access in a future method
 	req.requestTime = Date.now();
 	console.log(req.method, req.path);
-	// Calling next() makes it go to the next function that will handle the request
 	next();
 });
 
 app.use((err, req, res, next) => {
 	console.log('Error handling called');
-	// If want to print out the error stack, uncomment below
-	// console.error(err.stack)
-	// Updating the statusMessage with our custom error message (otherwise it will have a default for the status code).
 	res.statusMessage = err.message;
 
 	if (err.name === 'ValidationError') {
 		res.status(400).end();
 	} else {
-		// We could further interpret the errors to send a specific status based more error types.
 		res.status(500).end();
 	}
 });
 
-// SECTION get all notes
+// ANCHOR get all notes
 app.get('/api/notes', async function (req, res) {
-	// console.log(req.body);
 	const notes = await Note.find({ agent: req.session.userId });
 	const modifiedNotes = notes.map((mappedNote) => {
 		return mappedNote.toObject();
 	});
-	// res.json(notes);
 	console.log('🚀 ~ file: app.js ~ line 38 ~ modifiedNotes', modifiedNotes);
 	res.json(modifiedNotes.reverse());
-	// console.log(notes);
 });
 
-// SECTION create new note
+// ANCHOR create new note
 app.post('/api/notes', async function (req, res) {
 	console.log('Posted with body: ' + JSON.stringify(req.body));
 	const newNote = new Note({
@@ -98,11 +83,10 @@ app.post('/api/notes', async function (req, res) {
 	res.json(newNote);
 });
 
-// SECTION update a note
+// ANCHOR update a note
 app.put('/api/notes/:id', async function (req, res) {
 	let id = req.params.id;
 	console.log('PUT with id: ' + id + ', body: ' + JSON.stringify(req.body));
-	// This below method automatically saves it to the database
 	Note.findByIdAndUpdate(
 		id,
 		{
@@ -121,7 +105,7 @@ app.put('/api/notes/:id', async function (req, res) {
 	);
 });
 
-// SECTION delete a note
+// ANCHOR delete a note
 app.delete('/api/notes/:id', async function (req, res) {
 	let id = req.params.id;
 	console.log(id);
@@ -129,14 +113,14 @@ app.delete('/api/notes/:id', async function (req, res) {
 	res.json(afterDelete);
 });
 
-// SECTION get a user
+// ANCHOR get a user
 app.get('/api/users/loggedInUser', async function (req, res) {
 	const user = await User.findOne({ _id: req.session.userId });
 	console.log(user);
 	res.json(user);
 });
 
-// SECTION update a user
+// ANCHOR update a user
 app.put('/api/users', async function (req, res) {
 	const id = req.session.userId;
 	console.log(req.body);
@@ -159,42 +143,13 @@ app.put('/api/users', async function (req, res) {
 	);
 });
 
-// // update a user
-// app.put('/api/users/:id', async function (req, res) {
-// 	const id = req.params.id;
-// 	console.log(req.body);
-// 	User.findByIdAndUpdate(
-// 		id,
-// 		{
-// 			name: req.body.name,
-// 			email: req.body.email,
-// 			colorScheme: req.body.colorScheme,
-// 		},
-// 		function (err, result) {
-// 			if (err) {
-// 				console.log('ERROR: ' + err);
-// 				res.send(err);
-// 			} else {
-// 				res.sendStatus(204);
-// 			}
-// 		}
-// 	);
-// });
-
-// // delete a user
-// app.delete('/api/users/:id', async function (req, res) {
-// 	let id = req.params.id;
-// 	const afterDelete = await note.findByIdAndDelete(id);
-// 	res.json(afterDelete);
-// });
-
 function wrapAsync(fn) {
 	return function (req, res, next) {
 		fn(req, res, next).catch((e) => next(e));
 	};
 }
 
-// SECTION signup user
+// ANCHOR signup user
 app.post(
 	'/api/signup',
 	wrapAsync(async function (req, res) {
@@ -207,7 +162,7 @@ app.post(
 	})
 );
 
-// SECTION login user
+// ANCHOR login user
 app.post(
 	'/api/login',
 	wrapAsync(async function (req, res) {
@@ -223,7 +178,7 @@ app.post(
 	})
 );
 
-// SECTION logout user
+// ANCHOR logout user
 app.post(
 	'/api/logout',
 	wrapAsync(async function (req, res) {
@@ -231,21 +186,6 @@ app.post(
 		res.sendStatus(204);
 	})
 );
-
-// const upload = multer({dest: 'uploads/'})
-
-// // SECTION upload single image
-// app.post(
-// 	'/api/users/:id/file',
-// 	upload.single('image'),
-// 	wrapAsync(async function (req, res) {
-// 		// You can see the file details here – it also gets automatically saved into the uploads folder
-// 		// Again, this is an example of how this works but you would do something a little different in production.
-// 		console.log('File uploaded of length: ' + req.file.size);
-// 		console.dir(req.file);
-// 		res.json('File uploaded successfully');
-// 	})
-// );
 
 port = process.env.PORT || 5000;
 app.listen(port, () => {
